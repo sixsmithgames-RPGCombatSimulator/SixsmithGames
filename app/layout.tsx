387d3a798b80dfee15e722e588c33ff91758f2eb
Sixsmith Games <noreply@sixsmithgames.com>
2026-04-19 16:52:35 -0400

    added meta pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,28 @@ export default function RootLayout({
               })();
             `}
           </Script>
+          {/* Meta Pixel Code */}
+          <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1669983767681426');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img height="1" width="1" style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1669983767681426&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+          {/* End Meta Pixel Code */}
           <Navigation />
           <main style={{ minHeight: '100vh', paddingTop: '68px', overflowX: 'clip' }}>{children}</main>
           <Footer />
